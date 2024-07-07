939bec34b46e5d6877a13bdead4ffc654c0d0a2f
Adhityan K V <adhityan@hotmail.com>
2024-07-07 23:21:50 +0200

    clean pinecone embeddings when reloading

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,30 +4,37 @@ import { RAGApplicationBuilder, RAGApplication } from '@llm-tools/embedjs';
 import { PineconeDb } from '@llm-tools/embedjs/vectorDb/pinecone';
 
 let ragApplication: RAGApplication;
-const ragBuilder = new RAGApplicationBuilder()
-    .setSearchResultCount(30)
-    .addLoader({ type: 'Youtube', videoIdOrUrl: 's4pVFLUlx8g' })
-    .addLoader({ type: 'Youtube', videoIdOrUrl: 'JxpSuxBMVXo' })
-    .addLoader({ type: 'Web', urlOrContent: 'https://www.biography.com/business-leaders/steve-jobs' })
-    .addLoader({ type: 'Web', urlOrContent: 'https://en.wikipedia.org/wiki/Steve_Jobs' })
-    .setVectorDb(
-        new PineconeDb({
-            projectName: 'test',
-            namespace: 'dev',
-            indexSpec: {
-                serverless: {
-                    region: 'us-east-1',
-                    cloud: 'aws',
-                },
+const ragBuilder = new RAGApplicationBuilder().setSearchResultCount(30).setVectorDb(
+    new PineconeDb({
+        projectName: 'test',
+        namespace: 'dev',
+        indexSpec: {
+            serverless: {
+                region: 'us-east-1',
+                cloud: 'aws',
             },
-        }),
-    );
+        },
+    }),
+);
 
 export async function POST(req: Request) {
     const body = await req.json();
 
-    if (!ragApplication) ragApplication = await ragBuilder.build();
-    const result = await ragApplication.query(body.query);
+    if (!ragApplication) {
+        ragApplication = await ragBuilder.build();
+        await ragApplication.deleteAllEmbeddings(true);
 
+        await Promise.all([
+            ragApplication.addLoader({ type: 'Youtube', videoIdOrUrl: 's4pVFLUlx8g' }),
+            ragApplication.addLoader({ type: 'Youtube', videoIdOrUrl: 'JxpSuxBMVXo' }),
+            ragApplication.addLoader({
+                type: 'Web',
+                urlOrContent: 'https://www.biography.com/business-leaders/steve-jobs',
+            }),
+            ragApplication.addLoader({ type: 'Web', urlOrContent: 'https://en.wikipedia.org/wiki/Steve_Jobs' }),
+        ]);
+    }
+
+    const result = await ragApplication.query(body.query);
     return NextResponse.json({ result: result.content });
 }
